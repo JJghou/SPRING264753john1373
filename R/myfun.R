ecc286fd65f87a99ff42827e05583c5f631b3a53
Joeseph Johnson <Joeseph.F.Johnson-1@ou.edu>
2026-02-25 11:19:53 -0600

    Fixed Scatter Histogram To Properly Import From Graphics. Added Roxygen Skeleton To My Example Function (myfun)

--- R/myfun.R
+++ R/myfun.R
@@ -1,6 +1,11 @@
-getwd()
-dir()
-
+#' My Example Function
+#'
+#' @param x Numerical Value
+#'
+#' @returns Plot Of x Vs y, Where y Is X^2
+#' @export
+#'
+#' @examples 1
 myfun <- function(x) {
   y <- x^2
   plot(x,y)
